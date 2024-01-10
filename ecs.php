aa1ea86c9feb3793c794dd6de270740946614ecf
Nicolas Lemoine <nico.lemoine@gmail.com>
2024-01-10 12:06:59 +0100

    feat: update WpConfigStep
    - Add dyanmic parts (paths,  filename)
    - Coding standards
    - Support for symfony/dotenv:^7.0

---
+++ ecs.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
+use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
+use Symplify\CodingStandard\Fixer\Commenting\RemoveUselessDefaultCommentFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->parallel();
+
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+    ]);
+
+    $ecsConfig->sets([SetList::COMMON, SetList::PSR_12, SetList::CLEAN_CODE]);
+
+    $ecsConfig->ruleWithConfiguration(BinaryOperatorSpacesFixer::class, [
+        'operators' => [
+            '=>' => 'align_single_space',
+        ],
+    ]);
+
+    $ecsConfig->skip([
+        DeclareStrictTypesFixer::class            => null,
+        NotOperatorWithSuccessorSpaceFixer::class => null,
+        RemoveUselessDefaultCommentFixer::class   => null,
+        MethodChainingIndentationFixer::class     => null,
+    ]);
+};
--- src/WpConfigStep.php
+++ src/WpConfigStep.php
@@ -2,34 +2,38 @@
 
 namespace n5s\WpStarter;
 
+use Composer\Util\Filesystem;
 use WeCodeMore\WpStarter\Config\Config;
 use WeCodeMore\WpStarter\Step\BlockingStep;
-use WeCodeMore\WpStarter\Util\Paths;
 use WeCodeMore\WpStarter\Util\Locator;
+use WeCodeMore\WpStarter\Util\Paths;
 use WeCodeMore\WpStarter\Util\WpConfigSectionEditor;
 
 class WpConfigStep implements BlockingStep
 {
     private WpConfigSectionEditor $wpConfigSectionEditor;
 
+    private Filesystem $composerFilesystem;
+
     public function __construct(Locator $locator)
     {
         $this->wpConfigSectionEditor = $locator->WpConfigSectionEditor();
+        $this->composerFilesystem = $locator->composerFilesystem();
     }
 
     public function name(): string
     {
-        return 'custom-wp-config';
+        return 'n5s-wp-config';
     }
 
     public function success(): string
     {
-        return '<comment>custom-wp-config</comment> applied successfully.';
+        return '<comment>n5s-wp-config</comment> applied successfully.';
     }
 
     public function error(): string
     {
-        return 'custom-wp-config failed.';
+        return 'n5s-wp-config failed.';
     }
 
     public function allowed(Config $config, Paths $paths): bool
@@ -39,34 +43,44 @@ class WpConfigStep implements BlockingStep
 
     public function run(Config $config, Paths $paths): int
     {
-        $this->appendDotEnv();
+        $this->appendDotEnv($config, $paths);
+        $this->appendConstants($config, $paths);
         $this->appendSkipCache();
-        $this->removeThemeRegister();
+        $this->removeUnneededSections();
         return self::SUCCESS;
     }
 
     /**
      * Replace WP Starter's env loading with Dotenv's
      */
-    private function appendDotEnv(): void
+    private function appendDotEnv(Config $config, Paths $paths): void
     {
+        $from = $this->composerFilesystem->normalizePath($paths->wpParent());
+        /** @var string $envDir */
+        $envDir = $config[Config::ENV_DIR]->unwrapOrFallback($paths->root());
+        $envRelDir = $this->relPath($from, $envDir);
+
+        $envFile = $config[Config::ENV_FILE]->unwrapOrFallback('.env');
+
+        // @codingStandardsIgnoreStart
         $dotenv = <<<PHP
-// Prevent WP Starter to load from .env file
+// Prevent WP Starter to load from {$envFile} file
 \$_ENV['WPSTARTER_ENV_LOADED'] = true;
 
-// Only boot from .env files if not cached
-if (!file_exists(__DIR__ . '/.env.cached.php')) {
+// Only boot from {$envFile} files if not cached
+if (!is_file(WPSTARTER_PATH . WordPressEnvBridge::CACHE_DUMP_FILE)) {
     try {
         (new \Symfony\Component\Dotenv\Dotenv('WP_ENVIRONMENT_TYPE', 'WP_DEBUG'))
-            ->setProdEnvs(['production', 'prod', 'live', 'public'])
+            ->setProdEnvs(['production'])
             ->usePutenv()
-            ->bootEnv(realpath(__DIR__ . '/..') . '/.env', 'development');
+            ->bootEnv(realpath(__DIR__ . '{$envRelDir}/{$envFile}'), 'development');
     } catch (\Symfony\Component\Dotenv\Exception\PathException \$e) {
         http_response_code(500);
-        exit('Could not find a .env file. Please copy .env.example to .env and fill in the correct values.');
+        exit('Could not find a {$envFile} file. Please copy .env.example to {$envFile} and fill in the correct values.');
     }
 }
 PHP;
+        // @codingStandardsIgnoreEnd
         $this->wpConfigSectionEditor->append('AUTOLOAD', $dotenv);
     }
 
@@ -75,19 +89,66 @@ PHP;
      */
     private function appendSkipCache(): void
     {
+        // @codingStandardsIgnoreStart
         $skipCache = <<<PHP
 add_filter('wpstarter.skip-cache-env', function (\$skip, \$envName) {
     return \$skip || \$envName === 'development';
 }, 10, 2);
 PHP;
+        // @codingStandardsIgnoreEnd
+
         $this->wpConfigSectionEditor->append('BEFORE_BOOTSTRAP', $skipCache);
+    }
+
+    /**
+     * Append constants to wp-config.php
+     *
+     * Adds a all.php file to the ENV_BOOTSTRAP_DIR that is loaded on all environments.
+     */
+    private function appendConstants(Config $config, Paths $paths): void
+    {
+        $from = $this->composerFilesystem->normalizePath($paths->wpParent());
+        /** @var string $envBootstrapDir */
+        $envBootstrapDir = $config[Config::ENV_BOOTSTRAP_DIR]->unwrapOrFallback('');
+        if ($envBootstrapDir) {
+            $envBootstrapDir = $this->relPath($from, $paths->root($envBootstrapDir));
+        }
+
+        /** @var string $envDir */
+        $envDir = $config[Config::ENV_DIR]->unwrapOrFallback($paths->root());
+        $envRelDir = $this->relPath($from, $envDir);
+
+        $envDir = $envBootstrapDir ?: $envRelDir;
+
+        // @codingStandardsIgnoreStart
+        $constants = <<<PHP
+        \$alwaysIncludedConstants = realpath(__DIR__ . "{$envDir}/all.php");
+        \$hasAlwaysIncludedConstants = \$alwaysIncludedConstants && file_exists(\$alwaysIncludedConstants) && is_readable(\$alwaysIncludedConstants);
+        if (\$hasAlwaysIncludedConstants) {
+            require_once \$alwaysIncludedConstants;
+        }
+        unset(\$alwaysIncludedConstants, \$hasAlwaysIncludedConstants);
+PHP;
+        // @codingStandardsIgnoreEnd
+
+        $this->wpConfigSectionEditor->prepend('ENV_VARIABLES', $constants);
     }
 
     /**
      * Remove theme registering
      */
-    private function removeThemeRegister(): void
+    private function removeUnneededSections(): void
     {
         $this->wpConfigSectionEditor->delete('THEMES_REGISTER');
+        $this->wpConfigSectionEditor->delete('ADMIN_COLOR');
+    }
+
+    private function relPath(string $from, string $to, bool $bothDirs = true): string
+    {
+        $path = $this->composerFilesystem->normalizePath(
+            $this->composerFilesystem->findShortestPath($from, $to, $bothDirs)
+        );
+
+        return "/{$path}";
     }
 }
